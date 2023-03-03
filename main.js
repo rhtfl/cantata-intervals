8852bf5e0e223d4f28c3518e08b5706708401470
Daniil Kazantsev <35533685+rhtfl@users.noreply.github.com>
2023-03-03 15:15:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,384 @@
+let vostokAddresses = [
+    {
+        "sector": "Сектор 14",
+        "address": "г Москва, ул Большая Марфинская, д 1 к 2",
+        "lat": "55.826903",
+        "lon": "37.591809"
+    },
+    {
+        "sector": "Сектор 13",
+        "address": "10к1, Ярославское шоссе, Москва",
+        "lat": "55.853157",
+        "lon": "37.683338"
+    }
+    ,
+    {
+        "sector": "Сектор 12",
+        "address": "27, 3-я Парковая улица, Москва",
+        "lat": "55.793768",
+        "lon": "37.782225"
+    },
+    {
+        "sector": "Сектор 11",
+        "address": "5-я Сокольническая улица, Москва",
+        "lat": "55.785235",
+        "lon": "37.688027"
+    },
+    {
+        "sector": "Сектор 10",
+        "address": "29-33с1, Нижегородская улица, Москва",
+        "lat": "55.734293",
+        "lon": "37.709677"
+    },
+    {
+        "sector": "Сектор 9",
+        "address": "29к1, Братиславская улица, Москва",
+        "lat": "55.653710",
+        "lon": "37.766836"
+    },
+    {
+        "sector": "Сектор 8",
+        "address": "г Москва, ул Ясеневая, д 12 к 5",
+        "lat": "55.602566",
+        "lon": "37.734192"
+    },
+    {
+        "sector": "Сектор 8А",
+        "address": "2, Полевая улица, деревня Мисайлово",
+        "lat": "55.558015",
+        "lon": "37.819963"
+    },
+    {
+        "sector": "Сектор 8Б",
+        "address": "11, Промышленная улица, Домодедово",
+        "lat": "55.449644",
+        "lon": "37.767591"
+    },
+    {
+        "sector": "Сектор 9А",
+        "address": "12, микрорайон Южный, Котельники",
+        "lat": "55.643724",
+        "lon": "37.860513"
+    },
+    {
+        "sector": "Сектор 9Б",
+        "address": "22, улица Маяковского, Жуковский",
+        "lat": "55.595781",
+        "lon": "38.122740"
+    },
+    {
+        "sector": "Сектор 10А",
+        "address": "4, микрорайон Ольгино, Главная улица, Балашиха",
+        "lat": "55.749329",
+        "lon": "37.992305"
+    },
+    {
+        "sector": "Сектор 10Б",
+        "address": "8, улица Пушкина, Лосино-Петровский",
+        "lat": "55.865000",
+        "lon": "38.192314"
+    },
+    {
+        "sector": "Сектор 11А",
+        "address": "14, территория Северное Серково, городской округ Щёлково",
+        "lat": "55.899532",
+        "lon": "37.961412"
+    },
+    {
+        "sector": "Сектор 11Б",
+        "address": "2Ак73, Вокзальная улица, Фрязино",
+        "lat": "55.964337",
+        "lon": "38.046994"
+    },
+    {
+        "sector": "Сектор 12А",
+        "address": "12А, проспект Астрахова, Мытищи",
+        "lat": "55.925455",
+        "lon": "37.729979"
+    },
+    {
+        "sector": "Сектор 12Б",
+        "address": "Московская обл, г Пушкино, мкр Мамонтовка, ул Рабочая, д 19",
+        "lat": "56.000870",
+        "lon": "37.805904"
+    }
+];
+
+let centrAddresses = [
+    {
+        "sector": "Сектор 16",
+        "address": "19А, Хлебный переулок, Москва",
+        "lat": "55.755297",
+        "lon": "37.593740"
+    },
+    {
+        "sector": "Сектор 1",
+        "address": "Онежская улица 41",
+        "lat": "55.855982",
+        "lon": "37.510700"
+    },
+    {
+        "sector": "Сектор 2",
+        "address": "30А, улица Берзарина, Москва",
+        "lat": "55.790210",
+        "lon": "37.472324"
+    },
+    {
+        "sector": "Сектор 3",
+        "address": "г Москва, ул Большая Филёвская, д 47 к 1",
+        "lat": "55.740558",
+        "lon": "37.472827"
+    },
+    {
+        "sector": "Сектор 4",
+        "address": "12Д, проспект Вернадского, Москва",
+        "lat": "55.686269",
+        "lon": "37.511472"
+    },
+    {
+        "sector": "Сектор 5",
+        "address": "Россия, Москва, улица Стасовой, 14к2",
+        "lat": "55.712999",
+        "lon": "37.598106"
+    },
+    {
+        "sector": "Сектор 6",
+        "address": "3к2, Литовский бульвар, Москва",
+        "lat": "55.617195",
+        "lon": "37.535610"
+    },
+    {
+        "sector": "Сектор 7",
+        "address": "г Москва, ул Подольских Курсантов, д 1 стр 4",
+        "lat": "55.611180",
+        "lon": "37.611868"
+    },
+    {
+        "sector": "Сектор 1А",
+        "address": "4, посёлок Набережный, Береговая улица, Долгопрудный",
+        "lat": "55.911578",
+        "lon": "37.488008"
+    },
+    {
+        "sector": "Сектор 2А",
+        "address": "92к4, Юровская улица, Москва",
+        "lat": "55.898200",
+        "lon": "37.378073"
+    },
+    {
+        "sector": "Сектор 3А",
+        "address": "6, Ильинская улица, деревня Бузланово",
+        "lat": "55.771489",
+        "lon": "37.203647"
+    },
+    {
+        "sector": "Сектор 4А",
+        "address": "14к3, Саларьевская улица, поселение Московский",
+        "lat": "55.618857",
+        "lon": "37.411140"
+    },
+    {
+        "sector": "Сектор 5А",
+        "address": "29, улица Адмирала Лазарева, Москва",
+        "lat": "55.545650",
+        "lon": "37.528253"
+    },
+    {
+        "sector": "Сектор 1Б",
+        "address": "20к2, улица Крупской, Лобня",
+        "lat": "56.008772",
+        "lon": "37.475001"
+    },
+    {
+        "sector": "Сектор 2Б",
+        "address": "1934, Георгиевский проспект, Зеленоград",
+        "lat": "55.970977",
+        "lon": "37.178817"
+    },
+    {
+        "sector": "Сектор 3Б",
+        "address": "8, деревня Нефедьево",
+        "lat": "55.892991",
+        "lon": "37.158740"
+    },
+    {
+        "sector": "Сектор 4Б",
+        "address": "Московская обл, г Истра, деревня Новинки, ул Исаковская, д 8Б",
+        "lat": "55.822782",
+        "lon": "37.143675"
+    },
+    {
+        "sector": "Сектор 5Б",
+        "address": "9, коттеджный посёлок Усадьба",
+        "lat": "55.405445",
+        "lon": "37.264768"
+    },
+    {
+        "sector": "Сектор 6Б",
+        "address": "г Москва, поселение Филимонковское, ул Харлампиева, д 46",
+        "lat": "55.551008",
+        "lon": "37.312990"
+    },
+    {
+        "sector": "Сектор 7Б",
+        "address": "4, Пилотный переулок, Подольск",
+        "lat": "55.425491",
+        "lon": "37.516925"
+    }
+];
+
+
+(async () => {
+    let data_local_vostok;
+    
+    for (i = 0; i < vostokAddresses.length; i++ ){
+        let vostokGetInfo = 'https://deliveryback.vvdev.ru/api/delivery/delivery_calc?fullintervals=true&language=RU&lat_for_zone=' + vostokAddresses[i].lat + '&lon_for_zone=' + vostokAddresses[i].lon;
+        const getData = async () => {
+            const response = await fetch(vostokGetInfo,{
+              "headers": {
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site",
+                "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJ0eXBlIjoiY29tcGFuaWVzIiwic2VjcmV0IjoiMDViMDMxNGU4OWZjMjk5MTEwOTEzYmYxM2ExYWJiZjhhYmMwMjU2NCIsImlhdCI6MTYwNzAwMDM1MX0.E7pELaHqlRTppS3q1iQoZoj14qOtGSXXlNtl1HbhnFc"
+              },
+              "referrerPolicy": "strict-origin-when-cross-origin",
+              "body": null,
+              "method": "GET",
+              "mode": "cors",
+              "credentials": "omit"
+            });
+            const data = await response.json();
+            data_local_vostok = data;
+            return data;
+          };
+        
+          await getData();
+          
+        let getDaysVostok = data_local_vostok.delivery.courier.cantata;
+
+        const isoStr0 = getDaysVostok.days[0].date;
+        const date0 = new Date(isoStr0.slice(0, -1));
+
+        const isoStr1 = getDaysVostok.days[1].date;
+        const date1 = new Date(isoStr1.slice(0, -1));
+
+        console.log("Дата 0: " + date0.toLocaleDateString());
+        console.log("Интервалы 0: " + getDaysVostok.days[0].intervals);
+        console.log("");
+
+        let reusltBlock = document.getElementById('reusltBlock-Vostok');
+        reusltBlock.insertAdjacentHTML('beforeEnd', `<div class="col-4 pb-4"><div class="card shadow-sm h-100">
+            <div class="card-body">
+            <h5 class="card-title">${vostokAddresses[i].sector}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${vostokAddresses[i].address}</h6>
+            <p class="card-text mb-1">Дата: ${date0.toLocaleDateString()}</p>
+            <div class="sectora" id="sectoraV-${i}">${getDaysVostok.days[0].intervals}</div>
+            <p class="card-text mb-1"></br>Дата: ${date1.toLocaleDateString()}</p>
+            <div class="sectora" id="sectoraV-${i}">${getDaysVostok.days[1].intervals}</div>
+            </div>
+        </div></div>`)
+
+        let o = document.querySelectorAll("#sectoraV-" + i);
+        o[0].childNodes.forEach(e => {
+            let content;
+            if (e.nodeType == 3 && (content = e.textContent.trim())) {
+                let span = document.createElement("span");
+                span.className = "intervalText";
+                span.innerText = content;
+                e.parentNode.replaceChild(span, e);
+            }
+        });
+        o[1].childNodes.forEach(e => {
+            let content;
+            if (e.nodeType == 3 && (content = e.textContent.trim())) {
+                let span = document.createElement("span");
+                span.className = "intervalText";
+                span.innerText = content;
+                e.parentNode.replaceChild(span, e);
+            }
+        });
+        
+    }
+    
+    
+  })();
+
+  (async () => {
+    let data_local_centr;
+    
+    for (l = 0; l < centrAddresses.length; l++ ){
+        let CentrGetInfo = 'https://deliveryback.vvdev.ru/api/delivery/delivery_calc?fullintervals=true&language=RU&lat_for_zone=' + centrAddresses[l].lat + '&lon_for_zone=' + centrAddresses[l].lon;
+        const getData = async () => {
+            const response = await fetch(CentrGetInfo,{
+              "headers": {
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "cross-site",
+                "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJ0eXBlIjoiY29tcGFuaWVzIiwic2VjcmV0IjoiMDViMDMxNGU4OWZjMjk5MTEwOTEzYmYxM2ExYWJiZjhhYmMwMjU2NCIsImlhdCI6MTYwNzAwMDM1MX0.E7pELaHqlRTppS3q1iQoZoj14qOtGSXXlNtl1HbhnFc"
+              },
+              "referrerPolicy": "strict-origin-when-cross-origin",
+              "body": null,
+              "method": "GET",
+              "mode": "cors",
+              "credentials": "omit"
+            });
+            const data = await response.json();
+            data_local_centr = data;
+            return data;
+          };
+        
+          await getData();
+          
+        let getDaysCentr = data_local_centr.delivery.courier.cantata;
+
+        const isoStr0 = getDaysCentr.days[0].date;
+        const date0 = new Date(isoStr0.slice(0, -1));
+
+        const isoStr1 = getDaysCentr.days[1].date;
+        const date1 = new Date(isoStr1.slice(0, -1));
+
+        console.log("Дата 0: " + date0.toLocaleDateString());
+        console.log("Интервалы 0: " + getDaysCentr.days[0].intervals);
+        console.log("");
+
+        let reusltBlock = document.getElementById('reusltBlock-Centr');
+        reusltBlock.insertAdjacentHTML('beforeEnd', `<div class="col-4 pb-4"><div class="card shadow-sm h-100">
+            <div class="card-body">
+            <h5 class="card-title">${centrAddresses[l].sector}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${centrAddresses[l].address}</h6>
+            <p class="card-text mb-1">Дата: ${date0.toLocaleDateString()}</p>
+            <div class="sectora" id="sectoraC-${l}">${getDaysCentr.days[0].intervals}</div>
+            <p class="card-text mb-1"></br>Дата: ${date1.toLocaleDateString()}</p>
+            <div class="sectora" id="sectoraC-${l}">${getDaysCentr.days[1].intervals}</div>
+            </div>
+        </div></div>`)
+
+        let oo = document.querySelectorAll("#sectoraC-" + l);
+        oo[0].childNodes.forEach(e => {
+            let content;
+            if (e.nodeType == 3 && (content = e.textContent.trim())) {
+                let span = document.createElement("span");
+                span.className = "intervalText";
+                span.innerText = content;
+                e.parentNode.replaceChild(span, e);
+            }
+        });
+        oo[1].childNodes.forEach(e => {
+            let content;
+            if (e.nodeType == 3 && (content = e.textContent.trim())) {
+                let span = document.createElement("span");
+                span.className = "intervalText";
+                span.innerText = content;
+                e.parentNode.replaceChild(span, e);
+            }
+        });
+        
+    }
+    
+    
+  })();
+
