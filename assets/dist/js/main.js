b0fe45e2353dc98906b2f67fa942ae4f6d00eeb6
Daniil Kazantsev <35533685+rhtfl@users.noreply.github.com>
2023-04-13 03:20:19 +0300

    Глобальное обновление
    Переработана вся структура с нуля:
    1. добавлен новый ИМ
    2. вывод информации по всем дням
    3. экономия запросов
    4. вывод информации с сервера, для проверки на правильность данных
    5. адаптация под мобильные устройства

---
+++ assets/dist/js/main.js
@@ -0,0 +1,389 @@
+let AddressesList = [
+    [
+        {
+            "sector": "4",
+            "address": "улица Преображенский Вал, 17Бс4, Москва",
+            "lat": "55.793708",
+            "lon": "37.716791"
+        },
+        {
+            "sector": "4А",
+            "address": "коттеджный посёлок Лесной, 13, городской округ Щёлково, Московская область",
+            "lat": "55.879571",
+            "lon": "37.916963"
+        },
+        {
+            "sector": "4Б",
+            "address": "посёлок Литвиново, городской округ Щёлково, Московская область",
+            "lat": "56.009054",
+            "lon": "38.138191"
+        },
+        {
+            "sector": "5",
+            "address": "улица Плющева, 17к1, Москва",
+            "lat": "55.739275",
+            "lon": "37.762012"
+        },
+        {
+            "sector": "5А",
+            "address": "улица Разина, 6с1, Балашиха, Московская область",
+            "lat": "55.790150",
+            "lon": "37.930015"
+        },
+        {
+            "sector": "5Б",
+            "address": "Рогожская улица, 166, Ногинск, Богородский городской округ, Московская область",
+            "lat": "55.850494",
+            "lon": "38.422130"
+        },
+        {
+            "sector": "6",
+            "address": "Краснодонская улица, 39А, Москва",
+            "lat": "55.675077",
+            "lon": "37.749535"
+        },
+        {
+            "sector": "6А",
+            "address": "улица Нижняя Слобода, 197, село Остров, Ленинский городской округ, Московская область",
+            "lat": "55.581810",
+            "lon": "37.843247"
+        },
+        {
+            "sector": "6Б",
+            "address": "Центральная улица, 8, село Зюзино, Раменский городской округ, Московская область",
+            "lat": "55.676687",
+            "lon": "38.117539"
+        },
+        {
+            "sector": "7",
+            "address": "Домодедовская улица, 1к3, Москва",
+            "lat": "55.614643",
+            "lon": "37.707566"
+        },
+        {
+            "sector": "7А",
+            "address": "Белокаменное шоссе, 7, Видное, Ленинский городской округ, Московская область",
+            "lat": "55.551359",
+            "lon": "37.742941"
+        },
+        {
+            "sector": "7Б",
+            "address": "деревня Галушино, 2, Раменский городской округ, Московская область",
+            "lat": "55.447121",
+            "lon": "37.986268"
+        }
+    ],
+    [
+        {
+            "sector": "16",
+            "address": "Малая Бронная улица, 12с4, Москва",
+            "lat": "55.761234",
+            "lon": "37.597082"
+        },
+        {
+            "sector": "10",
+            "address": "улица Маршала Тухачевского, 58, Москва",
+            "lat": "55.784830",
+            "lon": "37.458526"
+        },
+        {
+            "sector": "10А",
+            "address": "деревня Барвиха, 78А, Одинцовский городской округ, Московская область",
+            "lat": "55.748736",
+            "lon": "37.290289"
+        },
+        {
+            "sector": "10Б",
+            "address": "коттеджный посёлок Монолит, 103, деревня Воронино, городской округ Истра, Московская область",
+            "lat": "55.788029",
+            "lon": "37.003673"
+        },
+        {
+            "sector": "9",
+            "address": "микрорайон Ленинские Горы, 1с10, район Раменки, Западный административный округ, Москва",
+            "lat": "55.702840",
+            "lon": "37.522387"
+        },
+        {
+            "sector": "9А",
+            "address": "деревня Переделки, 18, Одинцовский городской округ, Московская область",
+            "lat": "55.665086",
+            "lon": "37.329141"
+        },
+        {
+            "sector": "9Б",
+            "address": "деревня Митькино, 74, Одинцовский городской округ, Московская область",
+            "lat": "55.604687",
+            "lon": "37.101293"
+        },
+        {
+            "sector": "8",
+            "address": "Малая Юшуньская улица, 1с1, Москва",
+            "lat": "55.653782",
+            "lon": "37.594010"
+        },
+        {
+            "sector": "8А",
+            "address": "3-я Павлоградская улица, 6, Москва",
+            "lat": "55.544611",
+            "lon": "37.577849"
+        },
+        {
+            "sector": "8Б",
+            "address": "СНТ Росинка, 2, городской округ Подольск, Московская область",
+            "lat": "55.454351",
+            "lon": "37.402632"
+        }
+    ],
+    [
+        {
+            "sector": "1",
+            "address": "Кронштадтский бульвар, 24к1с1, Москва",
+            "lat": "55.847922",
+            "lon": "37.510089"
+        },
+        {
+            "sector": "1А",
+            "address": "Пятницкое шоссе, 48с1, Москва",
+            "lat": "55.861257",
+            "lon": "37.357860"
+        },
+        {
+            "sector": "1Б",
+            "address": "СНТ Горетовка-2, 53, городской округ Солнечногорск, Московская область",
+            "lat": "55.958991",
+            "lon": "37.114480"
+        },
+        {
+            "sector": "2",
+            "address": "Дубнинская улица, 12к2, Москва",
+            "lat": "55.864475",
+            "lon": "37.569683"
+        },
+        {
+            "sector": "2А",
+            "address": "площадь Собина, 3, Долгопрудный, Московская область",
+            "lat": "55.934059",
+            "lon": "37.514212"
+        },
+        {
+            "sector": "2Б",
+            "address": "улица Васильковое Поле, 001, деревня Мышецкое, городской округ Химки, Московская область",
+            "lat": "56.037398",
+            "lon": "37.362415"
+        },
+        {
+            "sector": "3",
+            "address": "улица Менжинского, 23к2, Москва",
+            "lat": "55.869137",
+            "lon": "37.659452"
+        },
+        {
+            "sector": "3А",
+            "address": "Малая Бородинская улица, 22, деревня Бородино, городской округ Мытищи, Московская область",
+            "lat": "55.922999",
+            "lon": "37.656928"
+        },
+        {
+            "sector": "3А2",
+            "address": "Ярославское шоссе, 116Ас1, Мытищи, Московская область",
+            "lat": "55.909812",
+            "lon": "37.782126"
+        },
+        {
+            "sector": "3Б",
+            "address": "Янтарная улица, 27, село Марфино, городской округ Мытищи, Московская область",
+            "lat": "56.073630",
+            "lon": "37.535457"
+        },
+        {
+            "sector": "3Б2",
+            "address": "1-я Серебрянская улица, 21, Пушкино, Московская область",
+            "lat": "56.016984",
+            "lon": "37.854530"
+        }
+    ]
+];
+
+function fetchData(url, sklad, index) {
+    fetch(url, {
+        "headers": {
+            "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJ0eXBlIjoiY29tcGFuaWVzIiwic2VjcmV0IjoiMDViMDMxNGU4OWZjMjk5MTEwOTEzYmYxM2ExYWJiZjhhYmMwMjU2NCIsImlhdCI6MTYwNzAwMDM1MX0.E7pELaHqlRTppS3q1iQoZoj14qOtGSXXlNtl1HbhnFc"
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        let intervals = data.delivery.courier.cantata.days;
+        // console.log(intervals);
+
+        let serverDateShow = document.querySelector('#list-server-' + sklad + "-" + index);
+        serverDateShow.insertAdjacentHTML('beforeend', `
+        <li class="list-group-item subtitle">Данные полученные с сервера для проверки:</li>
+        <li class="list-group-item">Наименование населенного пункта: ${data.name}</li>
+        <li class="list-group-item">Название склада с сервера: ${data.info.warehouse}</li>
+        <li class="list-group-item">Цена доставки: ${data.delivery.courier.cantata.price}&#8381</li>
+        <li class="list-group-item">Бесплатная доставка от: ${data.delivery.courier.cantata.sumRequired}&#8381</li>
+        `)
+
+        // sumRequired необходимая сумма для бесплатной доставки
+
+        let list = document.querySelector('#list-date-' + sklad + "-" + index);
+        // console.log(list);
+        for(a = 0; a < intervals.length; a++){
+            if (a < 2) {
+                list.insertAdjacentHTML('beforeEnd', `
+                <div class="list-group-item list-group-item-action list-date-item">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h6 class="mb-2">Дата: ${new Date(intervals[a].date.slice(0, -1)).toLocaleDateString()}</h6>
+                    </div>
+                    <div class="sectora">
+                    ${intervals[a].intervals}
+                    </div>
+                </div>
+                `)
+            }
+            if (a == 2) {
+                list.insertAdjacentHTML('beforeEnd', `
+                    <div class="collapse list-group-item list-group-item-action list-group-collapse" id="collapse-${sklad}-${index}">
+                        <div class="list-group-item list-group-item-action list-date-item">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h6 class="mb-2">Дата: ${new Date(intervals[a].date.slice(0, -1)).toLocaleDateString()}</h6>
+                        </div>
+                        <div class="sectora">
+                        ${intervals[a].intervals}
+                        </div>
+                        </div>                
+                    </div>
+                    <button class="list-group-item list-group-item-action btn-show-more" onclick="changeTextBtn(this);" id="btn-show-more" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-${sklad}-${index}" aria-expanded="false" aria-controls="collapseExample">Показать больше дней</button>
+                `)
+            }
+            let listCollapse = document.querySelector('#collapse-' + sklad + "-" + index);
+            if (a > 2) {
+                listCollapse.insertAdjacentHTML('beforeEnd', `
+                <div class="list-group-item list-group-item-action list-date-item">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h6 class="mb-2">Дата: ${new Date(intervals[a].date.slice(0, -1)).toLocaleDateString()}</h6>
+                    </div>
+                    <div class="sectora">
+                    ${intervals[a].intervals}
+                    </div>
+                </div>
+            `)
+            }
+            
+        }
+        changeViewIntervals();
+    })
+    .catch(error => {
+        console.error(error);
+        alert("Обратитесь к Даниилу, чтобы починил:с")
+    }
+    );
+}
+
+function changeViewIntervals() {
+    parentList = document.querySelectorAll('.sectora');
+    // console.log(parentList);
+    for(i=0;i<parentList.length;i++) {
+        parentList[i].childNodes.forEach(e => {
+            let content;
+            if (e.nodeType == 3 && (content = e.textContent.trim())) {
+                let span = document.createElement("span");
+                span.className = "intervalText";
+                span.innerText = content;
+                e.parentNode.replaceChild(span, e);
+            }
+        });
+    }
+    
+}
+
+function changeTextBtn(clickedElement){
+    // console.log(clickedElement.innerHTML);
+    clickedElement.innerHTML =
+    (clickedElement.innerHTML === 'Показать больше дней') ? clickedElement.innerHTML = 'Скрыть' : clickedElement.innerHTML = 'Показать больше дней';
+}
+
+let renderSectorsVostok = false;
+let renderSectorsCenter = false;
+let renderSectorsSever = false;
+function getIntervals(sklad) {
+    bannerContent.classList.add('my-0');
+    
+    if (sklad == 'Vostok' & renderSectorsVostok == false) {
+            let resultBlock = document.querySelector('#reusltBlock-Vostok');
+            for (i=0;i<AddressesList[0].length;i++) {
+                let link = 'https://deliveryback.vvdev.ru/api/delivery/delivery_calc?fullintervals=true&language=RU&lat_for_zone=' + AddressesList[0][i].lat + '&lon_for_zone=' + AddressesList[0][i].lon;
+                resultBlock.insertAdjacentHTML('beforeEnd', `
+                <div class="col-12 col-md-3 pb-3">
+                <div class="card">
+                    <div class="card-body" id="card-body-${sklad}-${i}">
+                        <h5 class="card-title">Сектор ${AddressesList[0][i].sector}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${AddressesList[0][i].address}</h6>
+                        <div class="list-group list-date" id="list-date-${sklad}-${i}">
+                        
+                        </div>
+                        <ul class="list-group list-group-flush list-server" id="list-server-${sklad}-${i}">
+
+                        </ul>
+                    </div>
+                </div>
+                </div>
+                `);
+                fetchData(link, sklad, i);
+            }
+            renderSectorsVostok = true;
+    }
+
+    if (sklad == 'Center' & renderSectorsCenter == false) {
+        let resultBlock = document.querySelector('#reusltBlock-Center');
+        for (i=0;i<AddressesList[1].length;i++) {
+            let link = 'https://deliveryback.vvdev.ru/api/delivery/delivery_calc?fullintervals=true&language=RU&lat_for_zone=' + AddressesList[1][i].lat + '&lon_for_zone=' + AddressesList[1][i].lon;
+                resultBlock.insertAdjacentHTML('beforeEnd', `
+                <div class="col-12 col-md-3 pb-3">
+                <div class="card">
+                    <div class="card-body" id="card-body-${sklad}-${i}">
+                        <h5 class="card-title">Сектор ${AddressesList[1][i].sector}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${AddressesList[1][i].address}</h6>
+                        <div class="list-group list-date" id="list-date-${sklad}-${i}">
+                        
+                        </div>
+                        <ul class="list-group list-group-flush list-server" id="list-server-${sklad}-${i}">
+
+                        </ul>
+                    </div>
+                </div>
+                </div>
+                `);
+                fetchData(link, sklad, i);
+        }
+        renderSectorsCenter = true;
+    }
+
+    if (sklad == 'Sever' & renderSectorsSever == false) {
+        let resultBlock = document.querySelector('#reusltBlock-Sever');
+        for (i=0;i<AddressesList[2].length;i++) {
+            let link = 'https://deliveryback.vvdev.ru/api/delivery/delivery_calc?fullintervals=true&language=RU&lat_for_zone=' + AddressesList[2][i].lat + '&lon_for_zone=' + AddressesList[2][i].lon;
+                resultBlock.insertAdjacentHTML('beforeEnd', `
+                <div class="col-12 col-md-3 pb-3">
+                <div class="card">
+                    <div class="card-body" id="card-body-${sklad}-${i}">
+                        <h5 class="card-title">Сектор ${AddressesList[2][i].sector}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${AddressesList[2][i].address}</h6>
+                        <div class="list-group list-date" id="list-date-${sklad}-${i}">
+                        
+                        </div>
+                        <ul class="list-group list-group-flush list-server" id="list-server-${sklad}-${i}">
+
+                        </ul>
+                    </div>
+                </div>
+                </div>
+                `);
+                fetchData(link, sklad, i);
+        }
+        renderSectorsSever = true;
+    }
+}
